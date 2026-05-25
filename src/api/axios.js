ae385a76d06c73124253fdd55cbb52669f863589
regulaharika2000 <regulaharika2000@gmail.com>
2026-05-25 16:51:27 +0530

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,22 +1,8 @@
 import axios from "axios";
 
-
-
 const api = axios.create({
   baseURL: "https://cravecart-5.onrender.com/api",
   withCredentials: true
-});
-
-
-// Add token automatically
-api.interceptors.request.use((req) => {
-  const token = localStorage.getItem("token");
-
-  if (token) {
-    req.headers.Authorization = `Bearer ${token}`;
-  }
-
-  return req;
 });
 
 export default api;
