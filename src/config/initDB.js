b5851fd286666fe45a4f9dfb194a9c915c56cbc1
regulaharika2000 <regulaharika2000@gmail.com>
2026-05-25 14:24:05 +0530

    Update initDB.js

--- src/config/initDB.js
+++ src/config/initDB.js
@@ -2,8 +2,8 @@ const fs = require("fs");
 const path = require("path");
 const db = require("./db");
 
-const schemaPath = path.join(__dirname, "../../../database/schema.sql");
-
+//const schemaPath = path.join(__dirname, "../../../database/schema.sql");
+const schemaPath = path.resolve(process.cwd(), "database", "schema.sql")
 const schema = fs.readFileSync(schemaPath, "utf8");
 
 db.exec(schema, (err) => {
