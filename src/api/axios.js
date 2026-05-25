a9451330995cc5fe21ab0ea2bb13fabea2c2ff60
regulaharika2000 <regulaharika2000@gmail.com>
2026-05-25 16:36:44 +0530

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://cravecart-5.onrender.com/api",
   withCredentials: true
 });
 
